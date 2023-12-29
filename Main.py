634a9ded4a4161271472c16d37a7ce8270439612
jacobahodge <153308257+jacobahodge@users.noreply.github.com>
2023-12-28 22:35:47 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,53 +1,53 @@
 tasks = []
 
 def addTask():
-  task = input ("Please enter a task: ")
-  task.append(task)
-  print(f"Task '{task}' has been added to the list")
+    task = input ("Please enter a task: ")
+    task.append(task)
+    print(f"Task '{task}' has been added to the list")
 
 def listTask():
-  if not tasks:
-    print("There are no tasks currently")
-else:
-  print("Current Tasks:")
-  for index, task in enumerate (tasks):
-    print(f"Tasks #{index}. {task}")
-
+    if not tasks:
+        print("There are no tasks currently")
+    else:
+        print("Current Tasks:")
+    for index, task in enumerate(tasks):
+        print(f"Tasks #{index}. {task}")
+        
 def deleteTask():
-  listTasks()
-  try: 
-  taskToDelete = int(input("Enter the number: "))
-    if taskToDelete < len(tasks) and >=0:
-      tasks.pop(taskToDelete)
-      print(f"Task {taskToDelete} has been removed")
-    else: 
-        print(f"Task #{taskToDelete} was not found")
-  except: 
-    print("Invalid input.") 
+    listTasks()
+    try: 
+        taskToDelete = int(input("Enter the number: "))
+        if taskToDelete < len(tasks) and taskToDelete >=0:
+            tasks.pop(taskToDelete)
+            print(f"Task {taskToDelete} has been removed")
+        else: 
+            print(f"Task #{taskToDelete} was not found")
+    except: 
+        print("Invalid input.") 
 
-if __name__ == "__main__";
+if __name__ == "__main__":
 ###Create a loop to run the app
-print("Welcome to my first To Do List app")
-while TRUE:
-  print("\n")
-  print("Please select on of the options")
-  print("-------------------------------")
-  print("1. Add a new task")
-  print("2. Delete a task")
-  print("3. List tasks")
-  print("4. Quit")
+    print("Welcome to my first To Do List app")
+while True:
+    print("\n")
+    print("Please select on of the options")
+    print("-------------------------------")
+    print("1. Add a new task")
+    print("2. Delete a task")
+    print("3. List tasks")
+    print("4. Quit")
 
 choice = input("Enter your choice: ")
 
 if (choice == "1"):
-  addTask()
+    addTask()
 elif (choice == "2"):
-  deleteTask()
+    deleteTask()
 elif (choice == "3"):
-  listTasks()
+    listTasks()
 elif (choice == "4"):
-  break
+    break
 else:
-  print("Invalid input. Please try again")
+    print("Invalid input. Please try again")
 
 print("Goodbye!")
